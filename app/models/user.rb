85121159c73ae376634bf8205c87f5a91316348c
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2021-12-04 09:11:29 +0000

    4[Add]

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,17 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
  
-  has_many :relationships, foreign_key: :follower_id
-  has_many :followings, through: :relationships, source: :followed
+  has_many :active_relationships,
+            class_name: "Relationship",
+            foreign_key: "follower_id",
+            dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
   
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :followed_id
-  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :passive__relationships,
+            class_name: 'Relationship',
+            foreign_key: :"followed_id",
+            dependent: :destroy
+  has_many :followers, through: :passive__relationships, source: :follower
  
   attachment :profile_image, destroy: false
 
@@ -20,11 +26,11 @@ class User < ApplicationRecord
   validates :introduction, length: { maximum: 50 }
 
   def follow(user_id)
-    relationships.create(followed_id: user_id)
+    active_relationships.create(followed_id: user_id)
   end
   
   def unfollow(user_id)
-    relationships.find_by(followed_id: user_id).destroy
+    active_relationships.find_by(followed_id: user_id).destroy
   end
   
   def following?(user)
