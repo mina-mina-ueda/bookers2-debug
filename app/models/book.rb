9582df46189345e27c7edd19c28776afa8c9730e
mina-mina-ueda <91769651+mina-mina-ueda@users.noreply.github.com>
2021-12-05 12:29:55 +0000

    5[Start]

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,4 +9,18 @@ class Book < ApplicationRecord
 	
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
+
+	def self.looks(search, word)
+	    if search == "perfect_match"
+	      @user = Book.where("title LIKE?", "#{word}")
+	    elsif search == "forward_match"
+	      @user = Book.where("title LIKE?", "#{word}")
+	    elsif search == "backword_match"
+	      @user = Book.where("title LIKE", "%#{word}")
+	    elsif search == "partial_match"
+	      @user = Book.where("title LIKE", "%#{word}")
+	    else
+	      @user = Book.all
+	    end
+	end  
 end
